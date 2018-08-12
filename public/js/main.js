9398bc3ebc9d920d63504375cd9bdf7c25dabc10
HurricaneInteractive <hurricane.interactive@gmail.com>
2018-08-12 22:15:38 +1000

    Compiled code

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
 "use strict";
-var KEY_USERNAME = 'username', KEY_LOCATION = 'location';
-var local_storage = window.localStorage, session_storage = window.sessionStorage, username = null, user_location = null, location_available = true, currentDate = new Date();
+var icons = {
+    "moon": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-moon\"><path d=\"M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z\"></path></svg>",
+    "sun": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-sun\"><circle cx=\"12\" cy=\"12\" r=\"5\"></circle><line x1=\"12\" y1=\"1\" x2=\"12\" y2=\"3\"></line><line x1=\"12\" y1=\"21\" x2=\"12\" y2=\"23\"></line><line x1=\"4.22\" y1=\"4.22\" x2=\"5.64\" y2=\"5.64\"></line><line x1=\"18.36\" y1=\"18.36\" x2=\"19.78\" y2=\"19.78\"></line><line x1=\"1\" y1=\"12\" x2=\"3\" y2=\"12\"></line><line x1=\"21\" y1=\"12\" x2=\"23\" y2=\"12\"></line><line x1=\"4.22\" y1=\"19.78\" x2=\"5.64\" y2=\"18.36\"></line><line x1=\"18.36\" y1=\"5.64\" x2=\"19.78\" y2=\"4.22\"></line></svg>"
+};
+var getIcon = function (icon) { return icons[icon]; };
+var KEY_USERNAME = 'username', KEY_LOCATION = 'location', SVG_STRUCTURE = "<svg class=\"feather\"><use xlink:href=\"img/feather-icons.svg#{{code}}\"/></svg>";
+var local_storage = window.localStorage, session_storage = window.sessionStorage, username = null, user_location = null, location_available = true, currentDate = new Date(), currentWeather = null;
+var createElement = function (dom_string) {
+    var template = document.createElement('template');
+    template.innerHTML = dom_string.trim();
+    return template.content.firstChild;
+};
 var getUserLocation = function (usr_pos) {
     if (navigator.geolocation && usr_pos === null) {
         return new Promise(function (resolve, reject) { return (navigator.geolocation.getCurrentPosition(resolve, reject)); });
@@ -11,6 +21,22 @@ var getUserLocation = function (usr_pos) {
         }
         else {
             return new Promise(function (resolve) { return resolve({}); });
+        }
+    }
+};
+var updateHeaderIcon = function () {
+    var domDestination = document.querySelector('header.appbar .details .datetime-location');
+    if (domDestination !== null) {
+        var currentSvg = document.querySelector('header.appbar .details .feather'), parent_1 = domDestination.parentNode;
+        if (parent_1 !== null) {
+            var hour = currentDate.getHours(), re = new RegExp('{{code}}', 'g'), icon = hour > 18 || hour < 6 ? 'moon' : 'sun', svg_struct = getIcon(icon), svg_dom = createElement(svg_struct);
+            console.log(svg_struct);
+            if (svg_dom !== null) {
+                if (currentSvg) {
+                    currentSvg.remove();
+                }
+                parent_1.insertBefore(svg_dom, domDestination);
+            }
         }
     }
 };
@@ -34,6 +60,7 @@ var setupApp = function () { return new Promise(function (resolve) {
             session_storage.setItem(KEY_LOCATION, JSON.stringify(user_location));
         }
         console.log('Get Weather Details...');
+        updateHeaderIcon();
     })
         .catch(function (e) { return console.error('Location', e); })
         .then(function () {
